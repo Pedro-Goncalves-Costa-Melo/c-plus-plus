cfd06b2d81f76511c39c1a021da5fd7ddcb833bf
Pedro Gonçalves Costa Melo <pedrogoncalvescm@gmail.com>
2023-05-17 12:15:00 -0300

    Update questao11.cpp

--- Lista de Estudos de Estruturas Condicionais/questao11-Aposentadoria/questao11.cpp
+++ Lista de Estudos de Estruturas Condicionais/questao11-Aposentadoria/questao11.cpp
@@ -2,20 +2,45 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     bool genero;
+    cin >> genero;
+
     int idade;
-    float tempoTrabalho;
+    cin >> idade;
 
-    cin>>genero>>idade>>tempoTrabalho;
-    
+    float anosTrabalho;
+    cin >> anosTrabalho;
+
+    float anosRestantes;
+
     if (genero)
+    //Calculo para Homem
     {
-        /* code */
-    }
-    
+        if (anosTrabalho<=20)
+        {
+            cout<<65<<endl;
+        }else{
 
+            anosRestantes = (35 - anosTrabalho)*1.3;
+            cout<<anosRestantes+idade<<endl;
+        }
+    }
+    else
+    //Calculo para Mulher
+    {
+        if (anosTrabalho<=15)
+        {
+            cout<<62<<endl;
+        }else{
+
+            anosRestantes = (30 - anosTrabalho)*1.3;
+            cout<<anosRestantes+idade<<endl;
+        }
+        
+    }
 
     return 0;
 }
