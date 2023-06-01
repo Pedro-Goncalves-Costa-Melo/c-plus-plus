6db6ca584c4cb14fc9292284be8d39a53f41e844
Pedro Gonçalves Costa Melo <pedrogoncalvescm@gmail.com>
2023-06-01 14:00:13 -0300

    questao18 resolvida

---
+++ Lista de estudos Matriz e strings/questao18 - Coringa e Charada Hash/questao18.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    unsigned n;
+    cin>>n;
+    string palavra;
+    int soma=0;
+    for (unsigned i = 0; i < n; i++)
+    {
+        const int UMPRIMO = 97;
+        const int M = 100;
+        long hash = 0;
+        cin>>palavra;
+
+        for (unsigned j = 0; j < palavra.size(); j++) 
+        {
+            hash = (UMPRIMO * hash + palavra[j]) % M;   
+        }
+        cout<<hash<<" ";
+        soma+=hash;
+    }
+    cout<<soma<<endl;
+    
+
+    
+
+    return 0;
+}
